dc155e19333a6403882db6a40d7a51a12820d6ae
Ignacio Molina Cuquerella <imolina@centeropenmiddleware.com>
2016-02-22 11:11:46 +0100

    Add setup.py for Pypi register

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Ignacio Molina Cuquerella'
+__email__ = 'imolina@centeropenmiddleware.com'
+
+setup(
+    name = 'RDFCrawlerService',
+    version = '1.0',
+    author = 'Ignacio Molina Cuquerella',
+    author_email = 'imolina@centeropenmiddleware.com',
+    description = 'A RESTful services for Python that explores and replicates '
+                  'a RDF topology.',
+    keywards = ['rdf', 'crawler', 'replication'],
+    url = 'https://github.com/ignaciomolina/RDFCrawler',
+
+    packages = find_packages(),
+    scripts= ['rdf_service.py']
+)
