6b556a22f5cfbceed49be495c7e53ad3a523c778
Ignacio Molina Cuquerella <imolina@centeropenmiddleware.com>
2016-02-23 11:25:14 +0100

    Fix setup.py name and version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ __email__ = 'imolina@centeropenmiddleware.com'
 
 setup(
     name = 'RDFCrawlerService',
-    version = '1.1',
+    version = '1.0',
     author = 'Ignacio Molina Cuquerella',
     author_email = 'imolina@centeropenmiddleware.com',
     description = 'A RESTful services for Python that explores and replicates '
                   'a RDF topology.',
     keywards = ['rdf', 'crawler', 'replication'],
     url = 'https://github.com/ignaciomolina/RDFCrawler',
-    download_url = 'https://github.com/ignaciomolina/RDFCrawler/tarball/1.1',
+    download_url = 'https://github.com/ignaciomolina/RDFCrawler/tarball/1.0',
     packages = find_packages(),
     scripts= ['rdf_service.py', 'rdf_crawler.py']
 )
