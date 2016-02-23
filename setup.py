7ebb56be5d022d47150b1e78dae658fee4e062b7
Ignacio Molina Cuquerella <imolina@centeropenmiddleware.com>
2016-02-23 11:06:46 +0100

    Fix setup.py dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __email__ = 'imolina@centeropenmiddleware.com'
 
 setup(
     name = 'RDFCrawlerService',
-    version = '1.0',
+    version = '1.1',
     author = 'Ignacio Molina Cuquerella',
     author_email = 'imolina@centeropenmiddleware.com',
     description = 'A RESTful services for Python that explores and replicates '
@@ -14,5 +14,5 @@ setup(
     url = 'https://github.com/ignaciomolina/RDFCrawler',
     download_url = 'https://github.com/ignaciomolina/RDFCrawler/tarball/1.1',
     packages = find_packages(),
-    scripts= ['rdf_service.py']
+    scripts= ['rdf_service.py', 'rdf_crawler.py']
 )
